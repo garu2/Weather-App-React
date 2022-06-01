0715ba7cb03f96576775fad3d528898b6d762db6
“Gary <elgafo.nakama@gmail.com>
2022-06-01 19:32:02 -0400

    remove all images and replace to links

--- src/components/Cities.jsx
+++ src/components/Cities.jsx
@@ -1,12 +1,3 @@
-import React from 'react';
-import Peru from "../../images/peru.png"
-import Colombia from "../../images/colombia.png"
-import Espania from "../../images/espania.png"
-import Bolivia from "../../images/bolivia.png"
-import Argentina from "../../images/argentina.png"
-import Mexico from "../../images/mexico.png"
-import Venezuela from "../../images/venezuela.png"
-import Ecuador from "../../images/ecuador.png"
 
 const Cities = ({ getWeather }) => { 
 
@@ -19,14 +10,30 @@ const Cities = ({ getWeather }) => {
         <section className="cities">
             <h2>Other Cities</h2>
             <ul>
-                <li><a href="#"><img data="Lima" src={ Peru } alt="" onClick={ChangeCity} /></a></li>
-                <li><a href="#"><img data="Bogota" src={ Colombia } alt="" onClick={ChangeCity} /></a></li>
-                <li><a href="#"><img data="Madrid" src={ Espania } alt="" onClick={ChangeCity}/></a></li>
-                <li><a href="#"><img data="La Paz" src={ Bolivia } alt="" onClick={ChangeCity}/></a></li>
-                <li><a href="#"><img data="Buenos Aires" src={ Argentina } alt="" onClick={ChangeCity} /></a></li>
-                <li><a href="#"><img data="Monterrey" src={ Mexico } alt="" onClick={ChangeCity} /></a></li>
-                <li><a href="#"><img data="Caracas" src={ Venezuela } alt="" onClick={ChangeCity} /></a></li>
-                <li><a href="#"><img data="Quito" src={ Ecuador } alt="" onClick={ChangeCity} /></a></li>
+                <li><a href="#">
+                    <img data="Lima" src="https://i.ibb.co/qgSvjm0/peru.png" alt="" onClick={ChangeCity} />
+                </a></li>
+                <li><a href="#">
+                    <img data="Bogota" src="https://i.ibb.co/ZxNwsGP/colombia.png" alt="" onClick={ChangeCity} />
+                </a></li>
+                <li><a href="#">
+                    <img data="Madrid" src="https://i.ibb.co/R2QhJ7r/espania.png" alt="" onClick={ChangeCity}/>
+                </a></li>
+                <li><a href="#">
+                    <img data="La Paz" src="https://i.ibb.co/qJdXCcX/bolivia.png" alt="" onClick={ChangeCity}/>
+                </a></li>
+                <li><a href="#">
+                    <img data="Buenos Aires" src="https://i.ibb.co/Br1LbfM/argentina.png" alt="" onClick={ChangeCity} />
+                </a></li>
+                <li><a href="#">
+                    <img data="Monterrey" src="https://i.ibb.co/dLtsckC/mexico.png" alt="" onClick={ChangeCity} />
+                </a></li>
+                <li><a href="#">
+                    <img data="Caracas" src="https://i.ibb.co/py2PHtk/venezuela.png" alt="" onClick={ChangeCity} />
+                </a></li>
+                <li><a href="#">
+                    <img data="Quito" src="https://i.ibb.co/yPhQ2pc/ecuador.png" alt="" onClick={ChangeCity} />
+                </a></li>
             </ul>
         </section>
     );
