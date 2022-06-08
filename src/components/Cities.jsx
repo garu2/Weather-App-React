3c2cfa65e1ccc94f86a6aee0cdf74f4acaa41d99
“Gary <elgafo.nakama@gmail.com>
2022-06-08 08:55:32 -0400

    FIX Cities component working

--- src/components/Cities.jsx
+++ src/components/Cities.jsx
@@ -1,8 +1,9 @@
 
 const Cities = ({ getWeather }) => { 
+    console.log('render');
 
-    const ChangeCity = e => {
-        const city = e.target.getAttribute('data');
+    const createHandleClick = city => {
+        //const city = e.target.getAttribute('data');
         getWeather(city);
     }
 
@@ -10,30 +11,33 @@ const Cities = ({ getWeather }) => {
         <section className="cities">
             <h2>Other Cities</h2>
             <ul>
-                <li><a href="#">
-                    <img data="Lima" src="https://i.ibb.co/qgSvjm0/peru.png" alt="" onClick={ChangeCity} />
+                <li>
+                    {/* <button onClick={ createHandleClick('Lima') }>
+                        <img data="Lima" src="https://i.ibb.co/qgSvjm0/peru.png" alt="" />
+                    </button> */}
+                    
+               </li>
+                {/* <li><a href="#">
+                    <img data="Bogota" src="https://i.ibb.co/ZxNwsGP/colombia.png" alt="" onClick={createHandleClick('Bogota')} />
                 </a></li>
                 <li><a href="#">
-                    <img data="Bogota" src="https://i.ibb.co/ZxNwsGP/colombia.png" alt="" onClick={ChangeCity} />
+                    <img data="Madrid" src="https://i.ibb.co/R2QhJ7r/espania.png" alt="" onClick={createHandleClick('Madrid')}/>
                 </a></li>
                 <li><a href="#">
-                    <img data="Madrid" src="https://i.ibb.co/R2QhJ7r/espania.png" alt="" onClick={ChangeCity}/>
+                    <img data="La Paz" src="https://i.ibb.co/qJdXCcX/bolivia.png" alt="" onClick={createHandleClick('La Paz')}/>
                 </a></li>
                 <li><a href="#">
-                    <img data="La Paz" src="https://i.ibb.co/qJdXCcX/bolivia.png" alt="" onClick={ChangeCity}/>
+                    <img data="Buenos Aires" src="https://i.ibb.co/Br1LbfM/argentina.png" alt="" onClick={createHandleClick('Buenos Aires')} />
                 </a></li>
                 <li><a href="#">
-                    <img data="Buenos Aires" src="https://i.ibb.co/Br1LbfM/argentina.png" alt="" onClick={ChangeCity} />
+                    <img data="Monterrey" src="https://i.ibb.co/dLtsckC/mexico.png" alt="" onClick={createHandleClick('Monterrey')} />
                 </a></li>
                 <li><a href="#">
-                    <img data="Monterrey" src="https://i.ibb.co/dLtsckC/mexico.png" alt="" onClick={ChangeCity} />
+                    <img data="Caracas" src="https://i.ibb.co/py2PHtk/venezuela.png" alt="" onClick={createHandleClick('Caracas')} />
                 </a></li>
                 <li><a href="#">
-                    <img data="Caracas" src="https://i.ibb.co/py2PHtk/venezuela.png" alt="" onClick={ChangeCity} />
-                </a></li>
-                <li><a href="#">
-                    <img data="Quito" src="https://i.ibb.co/yPhQ2pc/ecuador.png" alt="" onClick={ChangeCity} />
-                </a></li>
+                    <img data="Quito" src="https://i.ibb.co/yPhQ2pc/ecuador.png" alt="" onClick={createHandleClick('Quito')} />
+                </a></li> */}
             </ul>
         </section>
     );
