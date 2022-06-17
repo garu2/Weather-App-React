da63ca53a5727d5f73237c29f4b1c9930b10b43e
“Gary <elgafo.nakama@gmail.com>
2022-06-16 23:13:03 -0400

    FIX: remove conf of babel

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,4 @@
-module.exports = {
+/* module.exports = {
     presets: [
         '@babel/preset-react',
         [ 
@@ -10,4 +10,4 @@ module.exports = {
             },
         ],
     ],
-};
+}; */
