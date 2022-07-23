18ee8e4efd43034a4ec4f90791c4b80868aeace0
garu2 <elgafo.nakama@gmail.com>
2022-07-22 18:44:35 -0600

    FIX: Change Images extencions

--- src/components/Cities.jsx
+++ src/components/Cities.jsx
@@ -1,31 +1,20 @@
-//import Lima from "/lima.png";
-//import Bogota from "/bogota.png";
-//import Madrid from '/madrid.png';
-//import LaPaz from '/la paz.png';
-//import BuenosAires from '/buenos aires.png';
-//import Quito from '/quito.png';
-//import Caracas from '/caracas.png';
-//import Monterrey from '/monterrey.png';
-
 const Cities = ({ getWeather }) => { 
 
-    const cites = ['lima', 'bogota','madrid','la paz','buenos aires','quito','caracas','monterrey'];
-    //const flags = [Lima, Bogota, Madrid,LaPaz, BuenosAires,Quito, Caracas, Monterrey];
+    const flags = ['lima', 'bogota','madrid','la paz','buenos aires','quito','caracas','monterrey'];
 
     const createHandleClick = (city) => () => {
         getWeather(city);
     }
-    console.log('test repo');
 
     return ( 
         <section className="cities">
             <h2>Other Cities</h2>
             <ul>
                 {
-                cites.map((item, index) => (
+                flags.map((item, index) => (
                     <li key={index}>
                         <button onClick={createHandleClick(item)} >
-                            <img src={`/${item}.png`} alt={item} />            
+                            <img src={`/images/${item}.jpg`} alt={item} />            
                         </button>
                     </li>
                 ))
